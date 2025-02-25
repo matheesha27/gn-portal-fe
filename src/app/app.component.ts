8b442f894d9e07b628bf9c8e8acca81af97554ad
Matheesha Abeysekera <matheesha27@gmail.com>
2025-02-25 11:45:32 +0530

    Makes the sidenav bar responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 import { MainComponent } from "./main/main.component";
@@ -14,8 +14,17 @@ import { HeaderComponent } from './header/header.component';
 export class AppComponent {
   title = 'grama-niladhari-portal-fe';
   isLeftSidebarCollapsed = signal<boolean>(false);
+  screenwidth = signal<number>(window.innerWidth);
 
   changeLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
     this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenwidth.set(window.innerWidth);
+    if (this.screenwidth() < 768) {
+      this.isLeftSidebarCollapsed.set(true);
+    }
   }
 }
